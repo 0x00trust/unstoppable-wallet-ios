3c94a1ccfe6064a54e20d99b0b3ab8d501d52174
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2021-09-10 18:58:56 +0600

    Integrate new coin structure to Transactions module

--- UnstoppableWallet/UnstoppableWallet/Core/Adapters/BinanceAdapter.swift
+++ UnstoppableWallet/UnstoppableWallet/Core/Adapters/BinanceAdapter.swift
@@ -7,32 +7,32 @@ class BinanceAdapter {
     static let transferFee: Decimal = 0.000075
 
     private let binanceKit: BinanceChainKit
-    private let feePlatformCoin: PlatformCoin
-    private let platformCoin: PlatformCoin
+    private let feeCoin: PlatformCoin
+    private let coin: PlatformCoin
     private let asset: Asset
     private let transactionSource: TransactionSource
 
-    init(binanceKit: BinanceChainKit, symbol: String, feePlatformCoin: PlatformCoin, wallet: WalletNew) {
+    init(binanceKit: BinanceChainKit, symbol: String, feeCoin: PlatformCoin, wallet: WalletNew) {
         self.binanceKit = binanceKit
-        self.feePlatformCoin = feePlatformCoin
-        platformCoin = wallet.platformCoin
+        self.feeCoin = feeCoin
+        coin = wallet.platformCoin
         transactionSource = wallet.transactionSource
 
         asset = binanceKit.register(symbol: symbol)
     }
 
-//    private func transactionRecord(fromTransaction transaction: TransactionInfo) -> TransactionRecord {
-//        let fromMine = transaction.from == binanceKit.account
-//        let toMine = transaction.to == binanceKit.account
-//
-//        if fromMine && !toMine {
-//            return BinanceChainOutgoingTransactionRecord(source: transactionSource, transaction: transaction, feeCoin: feeCoin, coin: coin, sentToSelf: false)
-//        } else if !fromMine && toMine {
-//            return BinanceChainIncomingTransactionRecord(source: transactionSource, transaction: transaction, feeCoin: feeCoin, coin: coin)
-//        } else {
-//            return BinanceChainOutgoingTransactionRecord(source: transactionSource, transaction: transaction, feeCoin: feeCoin, coin: coin, sentToSelf: true)
-//        }
-//    }
+    private func transactionRecord(fromTransaction transaction: TransactionInfo) -> TransactionRecord {
+        let fromMine = transaction.from == binanceKit.account
+        let toMine = transaction.to == binanceKit.account
+
+        if fromMine && !toMine {
+            return BinanceChainOutgoingTransactionRecord(source: transactionSource, transaction: transaction, feeCoin: feeCoin, coin: coin, sentToSelf: false)
+        } else if !fromMine && toMine {
+            return BinanceChainIncomingTransactionRecord(source: transactionSource, transaction: transaction, feeCoin: feeCoin, coin: coin)
+        } else {
+            return BinanceChainOutgoingTransactionRecord(source: transactionSource, transaction: transaction, feeCoin: feeCoin, coin: coin, sentToSelf: true)
+        }
+    }
 
     private func adapterState(syncState: BinanceChainKit.SyncState) -> AdapterState {
         switch syncState {
@@ -142,65 +142,65 @@ extension BinanceAdapter: ISendBinanceAdapter {
 
 }
 
-//extension BinanceAdapter: ITransactionsAdapter {
-//
-//    var transactionState: AdapterState {
-//        switch binanceKit.syncState {
-//            case .synced: return .synced
-//            case .notSynced(let error): return .notSynced(error: error.convertedError)
-//            case .syncing: return .syncing(progress: 50, lastBlockDate: nil)
-//        }
-//    }
-//
-//    var lastBlockInfo: LastBlockInfo? {
-//        binanceKit.lastBlockHeight.map { LastBlockInfo(height: $0, timestamp: nil) }
-//    }
-//
-//    var lastBlockUpdatedObservable: Observable<Void> {
-//        binanceKit.lastBlockHeightObservable.map { _ in () }
-//    }
-//
-//    var transactionStateUpdatedObservable: Observable<Void> {
-//        binanceKit.syncStateObservable.map { _ in () }
-//    }
-//
-//    func transactionsObservable(coin: Coin?, filter: TransactionTypeFilter) -> Observable<[TransactionRecord]> {
-//        let binanceChainFilter: TransactionFilterType?
-//        switch filter {
-//            case .all: binanceChainFilter = nil
-//            case .incoming: binanceChainFilter = .incoming
-//            case .outgoing: binanceChainFilter = .outgoing
-//            default: binanceChainFilter = nil
-//        }
-//
-//        return asset.transactionsObservable(filterType: binanceChainFilter).map { [weak self] in
-//            $0.compactMap {
-//                self?.transactionRecord(fromTransaction: $0)
-//            }
-//        }
-//    }
-//
-//    func transactionsSingle(from: TransactionRecord?, coin: Coin?, filter: TransactionTypeFilter, limit: Int) -> Single<[TransactionRecord]> {
-//        let binanceChainFilter: TransactionFilterType?
-//
-//        switch filter {
-//            case .all: binanceChainFilter = nil
-//            case .incoming: binanceChainFilter = .incoming
-//            case .outgoing: binanceChainFilter = .outgoing
-//            default: return Single.just([])
-//        }
-//
-//        return binanceKit.transactionsSingle(symbol: asset.symbol, filterType: binanceChainFilter, fromTransactionHash: from?.transactionHash, limit: limit)
-//                .map { [weak self] transactions -> [TransactionRecord] in
-//                    transactions.compactMap { self?.transactionRecord(fromTransaction: $0) }
-//                }
-//    }
-//
-//    func rawTransaction(hash: String) -> String? {
-//        nil
-//    }
-//
-//}
+extension BinanceAdapter: ITransactionsAdapter {
+
+    var transactionState: AdapterState {
+        switch binanceKit.syncState {
+            case .synced: return .synced
+            case .notSynced(let error): return .notSynced(error: error.convertedError)
+            case .syncing: return .syncing(progress: 50, lastBlockDate: nil)
+        }
+    }
+
+    var lastBlockInfo: LastBlockInfo? {
+        binanceKit.lastBlockHeight.map { LastBlockInfo(height: $0, timestamp: nil) }
+    }
+
+    var lastBlockUpdatedObservable: Observable<Void> {
+        binanceKit.lastBlockHeightObservable.map { _ in () }
+    }
+
+    var transactionStateUpdatedObservable: Observable<Void> {
+        binanceKit.syncStateObservable.map { _ in () }
+    }
+
+    func transactionsObservable(coin: PlatformCoin?, filter: TransactionTypeFilter) -> Observable<[TransactionRecord]> {
+        let binanceChainFilter: TransactionFilterType?
+        switch filter {
+            case .all: binanceChainFilter = nil
+            case .incoming: binanceChainFilter = .incoming
+            case .outgoing: binanceChainFilter = .outgoing
+            default: binanceChainFilter = nil
+        }
+
+        return asset.transactionsObservable(filterType: binanceChainFilter).map { [weak self] in
+            $0.compactMap {
+                self?.transactionRecord(fromTransaction: $0)
+            }
+        }
+    }
+
+    func transactionsSingle(from: TransactionRecord?, coin: PlatformCoin?, filter: TransactionTypeFilter, limit: Int) -> Single<[TransactionRecord]> {
+        let binanceChainFilter: TransactionFilterType?
+
+        switch filter {
+            case .all: binanceChainFilter = nil
+            case .incoming: binanceChainFilter = .incoming
+            case .outgoing: binanceChainFilter = .outgoing
+            default: return Single.just([])
+        }
+
+        return binanceKit.transactionsSingle(symbol: asset.symbol, filterType: binanceChainFilter, fromTransactionHash: from?.transactionHash, limit: limit)
+                .map { [weak self] transactions -> [TransactionRecord] in
+                    transactions.compactMap { self?.transactionRecord(fromTransaction: $0) }
+                }
+    }
+
+    func rawTransaction(hash: String) -> String? {
+        nil
+    }
+
+}
 
 extension BinanceAdapter: IDepositAdapter {
 
